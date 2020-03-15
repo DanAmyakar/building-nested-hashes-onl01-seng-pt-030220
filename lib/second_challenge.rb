00a90a1da88c5888d4dfbc1eff2cda65526873d8
Daniel Amyakar <dan.rockit@gmail.com>
2020-03-15 00:50:25 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,14 +2,14 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :partiach => {},
+      :patriach => {},
       :matriach => {},
       :hero => {},
       :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
-      :partiach => {},
+      :patriach => {},
       :matriach => {},
       :heroine => {},
       :heroine_friends => []
